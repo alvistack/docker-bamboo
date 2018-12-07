29db30d3fa74df2953eb168a051c72737ac7c6f2
Wong Hoi Sing Edison <hswong3i@gmail.com>
2018-12-07 15:52:17 +0800

    move to ansible based deployment

---
+++ files/usr/local/bin/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o xtrace
+
+# Ensure required folders exist with correct owner:group
+mkdir -p $BAMBOO_HOME
+chown -Rf $BAMBOO_OWNER:$BAMBOO_GROUP $BAMBOO_HOME
+chmod 0755 $BAMBOO_HOME
+
+# Running Bamboo in foreground
+exec /etc/init.d/bamboo start -fg
