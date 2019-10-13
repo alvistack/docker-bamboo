e41c730d6bd4e2f61922c9a96922432eb83b2515
Wong Hoi Sing Edison <hswong3i@gmail.com>
2019-10-13 17:31:55 +0800

    chown $BAMBOO_OWNER:$BAMBOO_GROUP $BAMBOO_HOME

--- files/usr/local/bin/docker-entrypoint.sh
+++ files/usr/local/bin/docker-entrypoint.sh
@@ -10,7 +10,7 @@ fi
 # Allow the container to be stated with `--user`
 if [ "$1" = '/opt/atlassian/bamboo/bin/start-bamboo.sh' ] && [ "$(id -u)" = '0' ]; then
     mkdir -p $BAMBOO_HOME
-    chown -Rf $BAMBOO_OWNER:$BAMBOO_GROUP $BAMBOO_HOME
+    chown $BAMBOO_OWNER:$BAMBOO_GROUP $BAMBOO_HOME
     chmod 0755 $BAMBOO_HOME
     exec gosu $BAMBOO_OWNER "$BASH_SOURCE" "$@"
 fi
